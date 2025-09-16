9ad0553fb9386cf0c9e5d4fae3e50184cce7b335
Kodai <guangtianhuangda@gmail.com>
2025-09-16 17:44:05 +0900

    アイコンを更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/icon-192x192.png' },
+      { url: '/android-chrome-192x192.png' },
     ],
   },
 }
